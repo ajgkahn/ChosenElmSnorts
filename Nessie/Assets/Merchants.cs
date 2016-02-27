d77671b197a4fd3226a3d9fc735736e360722b2e
XCLS10R <benjhuff@gmail.com>
2016-02-27 04:38:41 -0500

    More Cleaning Up
    I got this

--- Nessie/Assets/Merchants.cs
+++ Nessie/Assets/Merchants.cs
@@ -6,6 +6,13 @@ struct Geocode{
 	public double lat;
 	public double lng;
 }
+struct Address{
+	public string city;
+	public string state;
+	public string streetName;
+	public string streetNumber;
+	public int zipCode;
+}
 
 public class Merchant : Object {
 	
@@ -13,24 +20,19 @@ public class Merchant : Object {
 	public Merchant(string[] input);
 	public string ToString();
 	
-	public string name{ get; set; }
-	public string id{ get; set; }
-	public string city{ get; set; }
-	public string state{ get; set; }
-	public string streetName{ get; set; }
-	public string streetNumber{ get; set; }
-	public int zipCode{ get; set; }
-	Geocode position{ get; set; }
+	public string name{ get; private set; }
+	public Address address{ get; private set; }
+	public Geocode position{ get; private set; }
 }
 Merchant::Merchant()
 {
 	name = "";
 	id = "";
-	city = "";
-	state = "";
-	streetName = "";
-	streetNumber = "";
-	zipCode = 0;
+	address.city = "";
+	address.state = "";
+	address.streetName = "";
+	address.streetNumber = "";
+	address.zipCode = 0;
 	position.lat = 0.0;
 	position.lng = 0.0;
 }
@@ -39,11 +41,11 @@ Merchant::Merchant(string[] input)
 	if (input.Length == 9){	
 		name = input[8];
 		id = input[0];
-		city = input[1];
-		state = input[2];
-		streetName = input[3];
-		streetNumber = input[4];
-		zipCode = Int32.TryParse(input[5]);
+		address.city = input[1];
+		address.state = input[2];
+		address.streetName = input[3];
+		address.streetNumber = input[4];
+		address.zipCode = Int32.TryParse(input[5]);
 		position.lat = float.TryParse(input[6]);
 		position.lng = float.TryParse(input[7]);
 	}
