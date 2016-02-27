51cb20c22534f09a9784ff36556cb38a6792133a
josheb76 <josheb76@vt.edu>
2016-02-27 03:16:00 -0500

    Added files via upload

---
+++ Nessie/Assets/NessieC.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+
+public class NessieC : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		
+		//StartCoroutine(getStuff());
+	}
+	
+	IEnumerator getStuff() {
+		//string url = "api.reimaginebanking.com/enterprise/accounts/56c66be6a73e492741507b2b?key=aa71fb9ed959b8325c48ad5b5892b755";
+		string url = "thebrandmans.net/joshtest/test.txt";
+		WWW www = new WWW(url);
+		yield return www;
+		
+		string raw = www.text.Substring(17,www.text.Length-23);
+		
+		Debug.Log(raw);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
