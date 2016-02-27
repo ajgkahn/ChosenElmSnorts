746e0498e0113997201c997efc633a1a11506f13
XCLS10R <benjhuff@gmail.com>
2016-02-27 04:29:51 -0500

    More Merchant-ness
    suck my nuts

--- Nessie/Assets/Merchants.cs
+++ Nessie/Assets/Merchants.cs
@@ -9,6 +9,8 @@ struct Geocode{
 
 public class Merchant : Object {
 	
+	public Merchant();
+	public Merchant(string[] input);
 	public string ToString();
 	
 	public string name{ get; set; }
@@ -20,7 +22,36 @@ public class Merchant : Object {
 	public int zipCode{ get; set; }
 	Geocode position{ get; set; }
 }
-
+Merchant::Merchant()
+{
+	name = "";
+	id = "";
+	city = "";
+	state = "";
+	streetName = "";
+	streetNumber = "";
+	zipCode = 0;
+	position.lat = 0.0;
+	position.lng = 0.0;
+}
+Merchant::Merchant(string[] input)
+{
+	if (input.Length == 9){	
+		name = input[0];
+		id = input[1];
+		city = input[2];
+		state = input[3];
+		streetName = input[4];
+		streetNumber = input[5];
+		zipCode = Int32.TryParse(input[6]);
+		position.lat = float.TryParse(input[7]);
+		position.lng = float.TryParse(input[8]);
+	}
+	else
+	{
+		System.Console.WriteLine("DON'T F***ING DO THAT. Also, you should probably pass an array with 9 elements");
+	}
+}
 string Merchant::ToString(){
 	string result;
 	result += "Name: ";
@@ -32,7 +63,7 @@ string Merchant::ToString(){
 	result += " ";
 	result += streetName;
 	result += ", ";
-	result += zipCode.ToString();
+	result += zipCode;
 	result += " Lat: ";
 	result += position.lat;
 	result += ", Long: ";
